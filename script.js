851100060a8972d2cd1d5c1b94d4b1fbe3ceee02
James <lawson.james.e@gmail.com>
2017-05-09 13:43:08 -0400

    palindrome

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 for (var i= 0; i<=55; i+5){
     console.log(i)
 }
+function isPal (word){
+    var ogWord = word;
+    if(ogWord === word.split('').reverse().join('')){
+        console.log('is palindrome')
+    } else{
+        console.log('nah')
+    }
+}
+
+isPal('RACECAR');
+isPal('jeremy irons');
+isPal('lolololololol');
